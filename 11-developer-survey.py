ee6a04f5ee36fc309fd97669f11a417de044d8e7
Tobias Knuth <tknuth@users.noreply.github.com>
2024-11-01 19:09:29 +0100

    added dev survey analysis

---
+++ 11-developer-survey.py
@@ -0,0 +1,48 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# Daten von https://survey.stackoverflow.co
+import pandas as pd
+
+pd.set_option("display.max_rows", 200)
+pd.set_option("display.max_columns", None)
+
+df = pd.read_csv("survey_results_public.csv")
+sc = pd.read_csv("survey_results_schema.csv").set_index("qname")
+
+# %%
+# Wie viel Programmiererfahrung haben Teilnehmer:innen nach Alterssegment in Jahren?
+# Interpretieren Sie die Ergebnisse. Finden Sie inkonsistente Werte?
+df["YearsCodeNumeric"] = pd.to_numeric(df.YearsCode, errors="coerce")
+df.groupby("Age").YearsCodeNumeric.describe()
+
+# %%
+# Welcher Anteil der Teilnehmer:innen arbeitet mit Python?
+df.LanguageHaveWorkedWith.str.contains("Python").value_counts(
+    dropna=False, normalize=True
+)
+
+# %%
+# Wie beliebt sind die verschiedenen Programmiersprachen unter den Teilnehmer:innen?
+df.LanguageHaveWorkedWith.fillna("n/a").str.split(";").explode().value_counts() / len(
+    df
+)
+
+# %%
+# Welcher Anteil der Teilnehmer:innen der Umfrage hat bereits mit ChatGPT gearbeitet?
+df.AISearchDevHaveWorkedWith.str.contains("ChatGPT").value_counts(
+    dropna=False, normalize=True
+)
